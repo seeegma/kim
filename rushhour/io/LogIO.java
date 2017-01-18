ab2729eab28b5a66539c60152d1118addfa2c983
Eric Walker <erictwalker18@gmail.com>
2017-01-18 00:11:22 -0600

    Added preliminary logio file

---
+++ rushhour/io/LogIO.java
@@ -0,0 +1,13 @@
+package rushhour.io;
+
+public class LogIO {
+    
+    /**
+     * Converter method
+     * @param dirName the localized directory name
+     * @return the array representation of the log file
+     */
+    public String[][] parseLogFile(String dirName) {
+        return "";
+    }
+}
