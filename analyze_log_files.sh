3bd6942d59923446a63309b522648fd8bc021262
dylandforbes <dylandforbes@gmail.com>
2017-01-28 13:40:52 -0600

    improve auto-data-grinding scripts

--- analyze_log_files.sh
+++ analyze_log_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+java rushhour.Main analyze --fields 
+echo
 for log_file in solve_logs/**/*.txt; do
 	puzzle_id=${log_file:11:-37}
 	if [[ ! -f puzzles/$puzzle_id.txt ]]; then
@@ -10,6 +12,6 @@ for log_file in solve_logs/**/*.txt; do
 	solve_id=${solve_id:0:-4}
 	printf $solve_id,
 	puzzle_file=puzzles/$puzzle_id.txt
-	java rushhour.Main analyze $puzzle_file $log_file
+	java rushhour.Main analyze --csv $puzzle_file $log_file
 	echo
 done
--- evaluate_puzzle_files.sh
+++ evaluate_puzzle_files.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+printf "puzzle_id,"
+java rushhour.Main evaluate --fields
+echo
 for puzzle_file in puzzles/*.txt; do
 	puzzle_id=${puzzle_file/puzzles\//}
 	puzzle_id=${puzzle_id%.txt}
-	echo $puzzle_id
+	printf "$puzzle_id,"
+	java rushhour.Main evaluate --csv "$puzzle_file"
+	echo
 done
