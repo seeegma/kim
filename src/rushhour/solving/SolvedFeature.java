8dd5c5bbde764d633864ff5652bfe7e359478635
dylandforbes <dylandforbes@gmail.com>
2017-03-14 14:17:09 -0500

    add trivial feature that's possibly quite useful

---
+++ src/rushhour/solving/SolvedFeature.java
@@ -0,0 +1,9 @@
+package rushhour.solving;
+
+import rushhour.core.Board;
+
+public class SolvedFeature implements Feature {
+	public double value(Board board) {
+		return board.isSolved() ? 0 : 1;
+	}
+}
