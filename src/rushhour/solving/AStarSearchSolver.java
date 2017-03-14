283c789b86489d559150670e081430c710cdd30c
dylandforbes <dylandforbes@gmail.com>
2017-03-13 21:48:25 -0500

    do the same thing for BFS. also remove statesVisited, replace with visited.size

--- src/rushhour/solving/AStarSearchSolver.java
+++ src/rushhour/solving/AStarSearchSolver.java
@@ -46,18 +46,16 @@ public class AStarSearchSolver extends BoardGraph implements Solver {
 		HashSet<Long> visited = new HashSet<>();
 		PriorityQueue<SearchNode> queue = new PriorityQueue<SearchNode>(100, this.comparator);
 		queue.offer(new SearchNode(this.getVertex(board)));
-		int statesVisited = 0;
 		while(!queue.isEmpty()) {
 			SearchNode current = queue.poll();
 			if(visited.contains(current.board.hash())) {
 				continue;
 			}
-			statesVisited++;
-			visited.add(current.board.hash());
 			if(current.vertex.board.isSolved()) {
 				// construct list from node tree
-				return new SolveResult(current.getPath(), current.vertex.board, statesVisited);
+				return new SolveResult(current.getPath(), current.vertex.board, visited.size());
 			}
+			visited.add(current.board.hash());
 			for(Edge edge : current.vertex.expand()) {
 				queue.offer(new SearchNode(edge.vertex, current, edge.move));
 			}
--- src/rushhour/solving/BreadthFirstSearchSolver.java
+++ src/rushhour/solving/BreadthFirstSearchSolver.java
@@ -2,6 +2,7 @@ package rushhour.solving;
 
 import rushhour.core.*;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.LinkedList;
 
@@ -9,16 +10,19 @@ public class BreadthFirstSearchSolver extends BoardGraph implements Solver {
 	public SolveResult getSolution(Board board) {
 		this.clear();
 		this.addVertex(board);
+		HashSet<Long> visited = new HashSet<>();
 		LinkedList<SearchNode> queue = new LinkedList<>();
 		queue.offer(new SearchNode(this.getVertex(board)));
-		int statesVisited = 0;
 		while(!queue.isEmpty()) {
 			SearchNode current = queue.poll();
-			statesVisited++;
+			if(visited.contains(current.board.hash())) {
+				continue;
+			}
 			if(current.vertex.board.isSolved()) {
 				// construct list from node tree
-				return new SolveResult(current.getPath(), current.vertex.board, statesVisited);
+				return new SolveResult(current.getPath(), current.vertex.board, visited.size());
 			}
+			visited.add(current.board.hash());
 			for(Edge edge : current.vertex.expand()) {
 				queue.offer(new SearchNode(edge.vertex, current, edge.move));
 			}
